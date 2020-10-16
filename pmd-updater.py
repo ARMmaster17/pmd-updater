ade2caacff72d9dde2de399be17e9f3ab8d43478
Joshua Zenn <wordman05@gmail.com>
2020-10-16 09:35:38 -0400

    Substitutes rule names in ruleset.xml from compiler output dump

--- pmd-updater.py
+++ pmd-updater.py
@@ -1,36 +1,36 @@
 import sys
 import re
 
-rulesetFile = None
-warningFile = None
 
-def getRulesetFileHandle(path):
-    return open(path, 'r')
+def get_change_rule(line):
+    match_obj = re.search("Use Rule name (.+) instead of the deprecated Rule name (.+)\\. PMD", line)
+    old_rule = match_obj.group(2)
+    new_rule = match_obj.group(1)
+    return old_rule, new_rule
 
-def getWarningFileHandle(path):
-    wFile = open(path, 'r')
-    return wFile.readlines()
 
-def parseArguments():
-    if len(sys.argv) < 1:
-        raise ValueError('Missing argument rulesetFilePath.')
-    rulesetFile = getRulesetFileHandle(sys.argv[1])
-    if len(sys.argv) >= 2:
-        warningFile = getWarningFileHandle(sys.argv[2])
-    else:
-        warningFile = getWarningFileHandle('warnings.txt')
-
-def performRulePathChange(line):
-    if (re.search("*Use Rule name * instead of the deprecated Rule name *\. PMD * will remove support for this deprecated Rule name usage\.", line) == None):
-        return
-    matchObj = re.search("Use Rule name (.*) instead of the deprecated Rule name (.*)\.", line)
-    oldRule = matchObj.group(2)
-    newRule = matchObj.group(1)
+def is_deprecate_warning(line):
+    return re.search(
+        "Use Rule name",
+        line) is not None
 
 def main():
-    parseArguments()
-    for line in warningFile:
-        performRulePathChange(line)
+    if len(sys.argv) != 3:
+        raise ValueError('Script requires two arguments.')
+    ruleset_file = open(sys.argv[1], 'r').readlines()
+    warning_file = open(sys.argv[2], 'r')
+    for line in warning_file:
+        print(".", end="")
+        if is_deprecate_warning(line):
+            print("+", end="")
+            old_string, new_string = get_change_rule(line)
+            print(f"{old_string}|{new_string}")
+            for line_number in range(len(ruleset_file)):
+                ruleset_file[line_number] = ruleset_file[line_number].replace(old_string, new_string)
+
+    for line in ruleset_file:
+        print(line)
+
 
 if __name__ == "__main__":
     main()
